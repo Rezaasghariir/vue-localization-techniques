f7882b3798c25f7673ce739384e99de203e7e43b
Reza Asghari <Reza.asghari.ir@gmail.com>
2022-07-16 23:54:01 +0430

    fix error

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
+//=============== for remove Console log ================
+const removeConsolePlugin = []
+if (process.env.NODE_ENV === 'production') {
+  removeConsolePlugin.push('transform-remove-console')
+}
+//=======================================================
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
-};
+  plugins: ['@babel/plugin-transform-runtime'],
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  overrides: [
+    {
+      test: '*.vue',
+      presets: [['@vue/app']]
+    },
+    {
+      test: '**/*.jsx',
+      presets: ['@babel/preset-react'],
+      plugins: ['@babel/plugin-transform-react-jsx']
+    }
+  ]
+}
